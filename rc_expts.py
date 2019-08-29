83c0d6174c1502649fdd44f31006264ee24d393a
ryanneely <ryan_m_neely@protonmail.com>
2019-08-28 17:46:51 -0700

    Updated list with all experiments currently on the hard disk; still waiting on 8x experiments from izumi from this past week

--- rc_expts.py
+++ rc_expts.py
@@ -4,5 +4,52 @@
 
 ##by Ryan Neely 8/28/19
 
+##anodal recruitment curves only
+anodal_RC_only_dsets = [
+    r"E:\7_1_19_RN1\rc",
+    r"E:\7_2_19_RN1\rc",
+    r"E:\7_8_19_IS1\rc",
+    r"E:\7_17_19_RN1\rc",
+    r"E:\7_16_19_RN1\rc" 
+]
 
+##cathodal recruitment curves only
+cathodal_RC_only_dsets = [
+    r"E:\7_22_19_RN1\rc",
+    r"E:\7_23_19_RN1\rc",
+    r"E:\7_24_19_RN1\rc"
+]
+##stimulation at 30Hz, 40 mins, 10mins post LPS, calibrated to 200% max response, 150us pulse width, anodal
+anodal_RC_200_dsets = [
+    r"E:\7_1_19_RN2\rc",
+    r"E:\6_24_19_RN1\rc",
+    r"E:\6_25_19_RN1\rc",
+    r"E:\6_26_19_RN1\rc",
+    r"E:\6_28_19_RN1\rc",
+    r"E:\7_1_19_RN1\rc",
+    r"E:\7_9_19_RN1\rc",
+    r"E:\7_8_19_RN1\rc",
+]
 
+##stimulation at 30Hz, 40 mins, 10m post-LPS, 150us PW, anodal, with amplitude set at 100% saturating evoked potential according to RC
+anodal_RC_100_dsets = [
+    r"E:\5_30_19_RN1\rc",
+    r"E:\6_3_19_RN1\rc",
+    r"E:\6_4_19_RN1\rc",
+    r"E:\6_5_19_RN1\rc",
+    r"E:\6_6_19_RN1\rc",
+    r"E:\6_7_19_RN1\rc"
+]
+
+##anodal stimulation, 20m pre-LPS, 5Hz, calibrated to 100% amplitude
+anodal_RC_5Hz_100_dsets = [
+    r"E:\7_18_19_RN1\rc",
+    # er.exp_7_18_19_IS1,
+    # er.exp_7_19_19_IS1,
+    # er.exp_7_17_19_IS1
+]
+
+##ex vivo, anodal stimulation, recruitment curves only
+anodal_rc_ex_vivo_dsets = [
+
+]
