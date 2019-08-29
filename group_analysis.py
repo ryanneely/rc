4e04a7ce607620b5681c8174c7f6deda1a71506c
ryanneely <ryan_m_neely@protonmail.com>
2019-08-28 18:06:02 -0700

    Adding files to process multiple groups of data, and to do plotting

---
+++ group_analysis.py
@@ -0,0 +1,6 @@
+##group_analysis.py
+
+##functions to look at the results of multiple experiments
+
+##by Ryan Neely 8/28/19
+
---
+++ plot.py
@@ -0,0 +1,5 @@
+##plot.py
+
+##functions to generate plots from processed data
+
+##by Ryan Neely 8/28/19
